fc4c9d51edcdb2ac997c98d5157ef7fb7abc2fd0
Pankaj Kumar <pankaj@Pankajs-MacBook-Air.local>
2022-01-31 07:05:25 +0530

    SpreadOperator

--- JavaScript/DS,MO,String.js
+++ JavaScript/DS,MO,String.js
@@ -16,14 +16,50 @@ const objectDestructure = ()=>{
 
     console.log(`Name = ${name} Age = ${age}, email = ${userEmail}`)
 
-    
+    //Function Parameter Destrucring
+    function printName({name:userName}){
+        console.log("printName " + userName)
+    }
+
+    printName(userData) //Passing Whole Object
 
 }
 
+function spreadOperator()
+{
+    console.log("Soread operator");
+    const userData = {
+        name: "Pankaj",
+        age:27,
+        info:{
+            email:"xyz@gmail.com",
+            linkedIn: "xyz.LinkedIn"
+        }
+    };
+
+    const userExtraData = {bestFriend:"Tom", ...userData}
+    console.log(userData);
+    console.log(userExtraData);
+
+    //working with array
+    console.log("\nArray Spread");
+    const hobbies = ["Chess","VitaminD",89];
+    const updateHobbies = [false,true, ...hobbies]; //sprea via shallow
+    console.log(hobbies);
+    console.log(updateHobbies);
+
+    //working with string
+    console.log("\nString spread")
+    const str = "Pankaj";
+    const letters = [...str];
+    console.log(str)
+    console.log(letters)
+}
 
 function main()
 {
-    objectDestructure()
+    //objectDestructure()
+    spreadOperator();
 }
 
 main()
