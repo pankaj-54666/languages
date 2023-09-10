c88f823b477ad36cad3c1e4e88cafeb2a36d17d8
= <pankaj.54666@gmail.com>
2023-09-10 17:40:54 +0530

    Printing & Compiling basics java

---
+++ java/HelloWorld/Basics.md
@@ -0,0 +1,7 @@
+
+Check Java Version:     java -version
+Compile :               javac HelloWorld.java
+Run:                    java HelloWorld (it will run HeloWorld.class)
+
+Compile & Run:          javac HelloWorld.java && java HelloWorld
+Make a short command:   TO-DO:
--- java/HelloWorld/HelloWorld.java
+++ java/HelloWorld/HelloWorld.java
@@ -1,9 +1,35 @@
+import static java.lang.System.out;
+import java.util.function.Consumer;
+
 
 class HelloWorld{
 
     public static void main(String args[])
     {
 
+        //Print Way1
         System.out.printf("Starting Main\n");
+
+        //Print Way2
+        out.printf("This make it c++ style printing\n");
+
+        //Print Way3
+        Consumer<String> printfFunction = System.out::printf; //Consumer is  a functional interface 
+
+        printfFunction.accept("Hello\n");
+
+        //Print Way4
+        /*  
+            VS Code -> Command Platter -> User Snippets -> select language as Java -> input below lines
+        
+        "Print using System.out.printf": {
+        "prefix": "sout",
+        "body": ["System.out.printf(\"$1\")"],
+        "description": "System.out.printf statement"}
+
+        then sout + tab in .java file to replace sout.
+        */
+       System.out.printf("After Setting on settings.json or java.json\n");
+
     }
 }
