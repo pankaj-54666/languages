3307d65e7aecd467d6d2c793aaf6a9aaa1e492c2
= <pankaj.54666@gmail.com>
2023-09-12 15:09:37 +0530

    minor change

--- java/OOP/OOP.java
+++ java/OOP/OOP.java
@@ -9,6 +9,7 @@ class OOP{
         /*
             AbstractClass vs Interface in Java
             private,public,protected and default scope in java
+            //https://github.com/mohammedabdulbari/Java-SE/blob/master/AbstractExample.java
 
             Methode Overriding in java.
         */
