e7796bc177cbb82e8e66fd0f8ee3743836ff3366
= <pankaj.54666@gmail.com>
2023-09-12 14:46:42 +0530

    OOP java

---
+++ java/OOP/OOP.java
@@ -0,0 +1,18 @@
+
+
+class OOP{
+    public static void main(String args[]){
+        System.out.printf("Starting Main\n");
+
+
+
+        /*
+            AbstractClass vs Interface in Java
+            private,public,protected and default scope in java
+
+            Methode Overriding in java.
+        */
+
+
+    }
+}
