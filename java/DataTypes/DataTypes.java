45245c72d02d48e8ae3f0587996287ec0af1a3c9
= <pankaj.54666@gmail.com>
2023-09-12 14:37:58 +0530

    1d and 2d array'

--- java/DataTypes/DataTypes.java
+++ java/DataTypes/DataTypes.java
@@ -9,6 +9,12 @@ class DataTypes{
             System.out.printf("%d ",arr[i]);
         }
         System.out.println("");
+
+        /* alternative is */
+        for(int x:arr)
+        {
+
+        }
     }
         
     /**
@@ -30,6 +36,63 @@ class DataTypes{
         System.out.printf("str = %s\n",str);
     }
 
+    void testArray()
+    {
+        //# 1D array Declaration
+        int arr1[] = new int[10]; //this is similary to c++ heap declaration
+        Arrays.fill(arr1,-1);
+
+        int arr2[]  = {1,2,3,4,5,6}; //shorthand notation
+
+        System.out.printf("Traversa array using for loop\t");
+        //traverse array : way1
+        for(int i=0;i<arr1.length;i++)
+        {
+            System.out.printf("%d ",arr1[i]);
+        }
+        System.out.printf("\n");
+
+        System.out.printf("Traversa array using auto similary\t");
+        //traverse array : way2 (Preferred)
+        for(int x:arr2)
+        {
+            System.out.printf("%d ",x);
+        }
+        System.out.printf("\n");       
+
+        //# 2D array 
+        
+        int mem[][] = { {1,2,3}, {2,4,6},{1,3,5}};
+        int mem2[][] = new int[10][101]; //2D
+        
+        //way1 print: preferred
+        System.out.printf("Printing 2D arrays\n");
+        for(int i=0;i<mem.length;i++)
+        {
+            for(int j=0;j<mem[i].length;j++)
+            {
+                System.out.printf("%d ",mem[i][j]);;
+            }
+            System.out.println();
+        }
+
+        //not preferred way
+        System.out.printf("\nPrinting 2D arrays\n");   
+        for(int x[]:mem)
+        {
+            for(int y:x)
+            {
+             System.out.printf("%d ",y);;
+            }
+            System.out.println();
+        }
+
+
+
+
+        String sarr[] = new String[3];
+    }
+
 
     /**
     Goal:
@@ -38,6 +101,7 @@ class DataTypes{
     (c) Sort Generic Array
     (d) Sort using comperator
     (e) Comperator using lambda expression, function expression and classs comperator.
+        Concolustion: we are using function defined on class Arrays.
     */
     void arraySorting()
     {
@@ -45,12 +109,18 @@ class DataTypes{
         //# Sort primitive array Array: normal + reverse
 
         int arr[] = {5,3,4,1,6,8,34,4,2};
+        int ara[] = new int[arr.length];
+        Arrays.fill(ara,-1); //fill all entry with -1
         
         System.out.println("\n\t::Before Sort\t");
-        printArray(arr);
+        // printArray(arr);
+        for(int x:arr)
+            System.out.printf("%d->",x);
+        System.out.printf("\n");
+        
 
         Arrays.sort(arr);
-        System.out.printf("After Sorting\n");
+        System.out.printf("\n\t::After Sorting\n");
         printArray(arr);
 
         //Task: How to sort in revese? as comperator does not accept primite types.
@@ -58,12 +128,12 @@ class DataTypes{
         //# Sort ArrayList
         // ArrayList<int> arr2 = new ArrayList<>(); //Will not work, as collection require Generics and not works with primitive 
 
-        ArrayList<Integer> arr3 = new ArrayList<>();
+        ArrayList<Integer> arr3 = new ArrayList<Integer>();
         arr3.add(3);
         arr3.add(2);
         arr3.add(5);
 
-        ArrayList<Integer> arr4  = new ArrayList<>(Arrays.asList(3,2,5));
+        ArrayList<Integer> arr4  = new ArrayList<Integer>(Arrays.asList(3,2,5));
 
         Collections.sort(arr4);
         System.out.println("SAfter soritng collection: " + arr4);
@@ -77,7 +147,6 @@ class DataTypes{
             return b.compareTo(a);
        };
 
-
         // Function Comperator
         Comparator<Integer> rComperator3 = new Comparator<Integer>() {
             @Override
@@ -86,9 +155,19 @@ class DataTypes{
             }
         };
 
+    //NOTE: in C++ we return true/false, but in java/javascript we return +ve,-ve and 0 value
+    /* Preferred */
+    Comparator<Integer> rComperator4 = (a,b) -> {
+            if(a<b) return -1;
+            if(b>a) return 1;
+            return 0;
+       };
+
         //TO-DO: add example for class comperator.
 
-        Collections.sort(arr4,rComperator1);
+        Collections.sort(arr4,rComperator4);
+
+        
 
     }
 
@@ -99,7 +178,7 @@ class DataTypes{
         ArrayList<String> arr = new ArrayList<String>(); //() calls the default constructor
 
         // List<String> list = new ArrayList<String>() | new LinkedList<>() ; alternative syntax using parent class List
-        
+
         arr.add("Hello");
         arr.add("Kumar");
         arr.add(1,"Middle"); //add element at index 1
@@ -329,6 +408,74 @@ class DataTypes{
         System.out.printf("Updated set entry is: " + s);
     }
 
+    void testString()
+    {
+        String s1 = "I am a immutable string, i cannot be edited";
+        String s2 = new String("Alternatie and more streamline with java decralartion");
+
+        StringBuffer s3 = new StringBuffer("I am editable string");
+
+        //Note on builder: not thread-safe, but fast on single-thread scenerio
+        StringBuilder s4 = new StringBuilder("String using string builder"); 
+
+        System.out.println("s1: "+ s1 + "\ns2: " + s2 + "\ns3: " + s3 +"\ns4: "+s4);
+
+        // s3 = s3 + "additiong"; //NOTE: this style will not work as s3 and "addition" are of different type
+        s3.append("addition");
+
+        String s5 = "Hello" + "There";
+
+        System.out.println("s1: "+ s1 + "\ns2: " + s2 + "\ns3: " + s3 +"\ns4: "+s4 + "\ns5: " +s5);
+
+
+        /*
+            .concat : for immutatble string(return new string)
+            .append : for mutalbe string(modify original string)
+        */
+        
+
+        // # Strign Construction ways 
+        //https://docs.oracle.com/javase/8/docs/api/java/lang/String.html#constructor.summary
+
+        String a1 = "Java Program";
+        String a2 = new String("Java");
+
+        char c[] = {'H','e','l','l','0'};
+        String a3 = new String(c);
+
+        byte b[] = {65,66,67,68}; //ascii code (ABCD)
+        String a4 = new String(b);
+
+        //"Java" != new String("Java") => Reason why
+
+        // # String Methodes
+
+        String s = new String("Test String");
+        s.length();
+        s.charAt(3);
+        s.equals("Test String");
+        s.equalsIgnoreCase("test");
+        s.compareTo("test");
+        s.trim();
+
+        s.substring(1);
+        s.substring(4,5);
+        s.toLowerCase();
+        s.toUpperCase();
+        s.toLowerCase();
+
+        s.trim();
+        s.startsWith("Tes");
+        s.endsWith("ing");
+
+        String regexExpression  = "[a-z]*";
+        s.matches(regexExpression);
+        //other less used at: https://docs.oracle.com/javase/8/docs/api/java/lang/String.html#method.summary
+        
+
+
+
+    }
     public static void main(String args[])
     {
         System.out.printf("Starting Main\n");
@@ -339,9 +486,11 @@ class DataTypes{
         // Call the primitiveTypes method on the instance
         // object.primitiveTypes();
 
+        object.testArray();
+
         // object.testArrayList();
 
-        // object.arraySorting();
+        object.arraySorting();
 
         // object.testMap();
 
@@ -350,7 +499,9 @@ class DataTypes{
         // object.testQueue();
         // object.testStack();
 
-        object.testSet();
+        // object.testSet();
+
+        object.testString();
 
         System.out.printf("\n");
 
