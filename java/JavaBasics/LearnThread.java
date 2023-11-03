ba4df2c0c949925d5f42c4d198c7f51ac552a230
= <pankaj.54666@gmail.com>
2023-11-03 17:58:14 +0530

    update

--- java/JavaBasics/LearnThread.java
+++ java/JavaBasics/LearnThread.java
@@ -1,6 +1,5 @@
 package com.examples.thread;
 
-//Thread using extends
 class ThreadTestOne extends Thread{
 
     @Override
@@ -9,7 +8,6 @@ class ThreadTestOne extends Thread{
     }
 }
 
-//thread using interface
 class ThreadTestTwo implements Runnable{
 
     @Override
@@ -18,19 +16,26 @@ class ThreadTestTwo implements Runnable{
     }
 };
 
-//javac -d . LearnThread.java && java com/examples/thread/ThreadTest
+//javac -d . LearnThread.java && java com/examples/thread/LearnThread
 public class LearnThread{
+
 
 
     public static void main(String args[]){
 
+        //Task1: Thread using extends
         ThreadTestOne obj1 = new ThreadTestOne();
         obj1.run();
 
 
-
+        //Task2: Thread using implement 
         ThreadTestTwo obj2 = new ThreadTestTwo();
         obj2.run();
+
+        //Task3L Thread Methodes
+
+        System.out.printf("threadID: %d, threadName: %s, priority: %d\n",obj1.getId(),obj1.getName(),obj1.getPriority());
+        
 
     }
 }
