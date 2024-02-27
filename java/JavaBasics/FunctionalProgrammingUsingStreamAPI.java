b91985f67f71dae02912326714d99949ce6dfacf
Pankaj Kumar <pankaj@Pankajs-MacBook-Air.local>
2024-02-27 23:12:32 +0530

    added stream api example

---
+++ java/JavaBasics/FunctionalProgrammingUsingStreamAPI.java
@@ -0,0 +1,16 @@
+package com.functional;
+import java.util.function.Function;
+
+//javac -d . FunctionalProgrammingUsingStreamAPI.java && java com/functional/FunctionProgrammingUsingStreamAPI
+class FunctionProgrammingUsingStreamAPI{
+
+
+
+
+    public static void main(String args[])
+    {
+        System.out.printf("Inside FunctionProgrammingUsingStreamAPI main\n");
+
+
+    }
+}
