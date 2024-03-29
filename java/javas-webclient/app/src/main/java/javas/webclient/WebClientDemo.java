2f63a3378087958f9d403223f4ee915edfe90aee
Pankaj Kumar <pankaj@Pankajs-MacBook-Air.local>
2024-03-29 23:20:06 +0530

    post reuest , get request with webclient

--- java/javas-webclient/app/src/main/java/javas/webclient/WebClientDemo.java
+++ java/javas-webclient/app/src/main/java/javas/webclient/WebClientDemo.java
@@ -1,35 +1,105 @@
 package javas.webclient;
 
-
-import lombok.extern.slf4j.Slf4j;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import javas.webclient.exchange.ProductRequestVo;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.stereotype.Component;
+import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
-@Component
+//@Component
 //@Slf4j //TODO: check why log.info not working after above
 public class WebClientDemo {
-    private static final Logger logger = LogManager.getLogger("HelloWorld");
-    public void demo()
+    //How to use logger https://logging.apache.org/log4j/2.x/manual/api.html
+    private static final Logger logger = LogManager.getLogger(WebClientDemo.class);
+//    public static Logger lprintf = LogManager.getFormatterLogger("Foo");
+
+    public void postSync()  {
+        String baseUrl = "https://dummyjson.com";
+
+//        WebClient client = WebClient.create(baseUrl);
+        WebClient client = WebClient.builder()
+                .baseUrl(baseUrl)
+                .build();
+
+        ProductRequestVo requestBody = new ProductRequestVo("BMW Pencisl");
+
+        Gson gson = new Gson(); //way1 to convert to json
+        ObjectMapper objectMapper = new ObjectMapper(); //way2 to convert to json
+
+        try {
+            //https://docs.spring.io/spring-framework/reference/web/webflux-webclient/client-body.html
+            String responseBody = client.post()
+                    .uri("/products/add")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .bodyValue(requestBody)     //Was not working when jackson depedency was not properly configured
+//                  .bodyValue(gson.toJson(requestBody)) //preferred2
+//                  .bodyValue(objectMapper.writeValueAsString(requestBody))
+                    .retrieve()
+                    .bodyToMono(String.class)
+                    .block();
+
+            System.out.println(responseBody);
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void getSync()
     {
 
-        String url = "www.google.com";
+        String url = "https://dummyjson.com/carts";
+
         WebClient client = WebClient.builder()
                 .baseUrl(url)
                 .build();
 
-        Mono<String> response = client.post()
+        String sres = client.get()
+                .retrieve()
+                .bodyToMono(String.class)
+//                .log()
+                .block();
+
+//        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+//        String sjson = gson.toJson(sres);
+//
+//        logger.info("sjson: {}",sjson);
+    }
+    public void getAsync()
+    {
+
+        String url = "https://dummyjson.com/carts"; //https://github.com/Ovi/DummyJSON
+
+        WebClient client = WebClient.builder()
+                .baseUrl(url)
+                .build();
+
+        Mono<String> response = client.get()
                 .retrieve()
                 .bodyToMono(String.class);
 
+        //TODO: this does not get called, as main thread is exited & does not wait for Mono response to be finished
+        response.subscribe(
+                responseBody -> logger.info("Response body: {}", responseBody),
+                error -> logger.error("Error happened: ", error)
+        );
 
-        response.subscribe(responseBody ->
+    }
 
-                logger.info("responseBody: ",responseBody),
-                error -> {
-                logger.error("Error happended: ",error);
-                });
+    public void test()
+    {
+        try {
+            logger.info("Enter::test");
+
+//        postSync();
+            postSync();
+//        getSync();
+        }catch (Exception e){
+            logger.info("Error: ",e);
+        }
     }
 }
---
+++ java/javas-webclient/app/src/main/java/javas/webclient/exchange/ProductRequestVo.java
@@ -0,0 +1,12 @@
+package javas.webclient.exchange;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductRequestVo {
+    private String title;
+}
