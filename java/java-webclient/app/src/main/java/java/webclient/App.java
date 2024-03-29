441dbf0a8db9c55e906cb34af4eb89f70616245a
Pankaj Kumar <pankaj@Pankajs-MacBook-Air.local>
2024-03-29 18:08:38 +0530

    added some test code

--- java/java-webclient/app/src/main/java/java/webclient/App.java
+++ java/java-webclient/app/src/main/java/java/webclient/App.java
@@ -10,5 +10,8 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
+
+        WebClientDemo webClientDemo = new WebClientDemo();
+        webClientDemo.demo();
     }
 }
---
+++ java/java-webclient/app/src/main/java/java/webclient/WebClientDemo.java
@@ -0,0 +1,35 @@
+package java.webclient;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@Component
+//@Slf4j //TODO: check why log.info not working after above
+public class WebClientDemo {
+    private static final Logger logger = LogManager.getLogger("HelloWorld");
+    public void demo()
+    {
+
+        String url = "www.google.com";
+        WebClient client = WebClient.builder()
+                .baseUrl(url)
+                .build();
+
+        Mono<String> response = client.post()
+                .retrieve()
+                .bodyToMono(String.class);
+
+
+        response.subscribe(responseBody ->
+
+                logger.info("responseBody: ",responseBody),
+                error -> {
+                logger.error("Error happended: ",error);
+                });
+    }
+}
