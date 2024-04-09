2d380d27135548eca0ace1389f968ce239c63a67
= <pankaj.54666@gmail.com>
2024-04-09 09:04:43 +0530

    renamed s12 to routing-project

---
+++ angular/archieve/s12-routing-project/course-project-routing-final/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
+    DropdownDirective,
+    RecipeStartComponent,
+    RecipeEditComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  providers: [ShoppingListService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
